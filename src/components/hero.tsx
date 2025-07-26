96bf642763f6e4e96dba5bbcf5bd3da2abf80b7f
Gd. Megh <gdmegh@gmail.com>
2025-07-26 04:49:40 +0000

    background effect will be more down

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <section className="relative h-[90vh] min-h-[700px] flex items-center justify-center text-center overflow-hidden">
       <div className="absolute inset-0 z-0">
-        <div className="absolute -bottom-[15%] left-1/2 -translate-x-1/2 w-[100vw] h-[50vh] max-w-[1200px] bg-primary/20 rounded-full blur-3xl"></div>
+        <div className="absolute -bottom-[25%] left-1/2 -translate-x-1/2 w-[100vw] h-[50vh] max-w-[1200px] bg-primary/20 rounded-full blur-3xl"></div>
       </div>
       <div className="container mx-auto px-4 z-10">
         <div className="max-w-4xl mx-auto flex flex-col items-center">
