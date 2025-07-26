d927d2a766c0ec4de9384a5d9bda7eb02212e150
Gd. Megh <gdmegh@gmail.com>
2025-07-26 04:54:44 +0000

    Let's Talk Your Idea

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   return (
     <section className="relative h-[90vh] min-h-[700px] flex items-center justify-center text-center overflow-hidden">
       <div className="absolute inset-0 z-0">
-        <div className="absolute -bottom-[80%] left-1/2 -translate-x-1/2 w-[100vw] h-[50vh] max-w-[1200px] bg-primary/50 rounded-full blur-3xl"></div>
+        <div className="absolute -bottom-[95%] left-1/2 -translate-x-1/2 w-[100vw] h-[50vh] max-w-[1200px] bg-primary/80 rounded-full blur-3xl"></div>
       </div>
       <div className="container mx-auto px-4 z-10">
         <div className="max-w-4xl mx-auto flex flex-col items-center">
@@ -16,7 +16,7 @@ export default function Hero() {
           <h1 className="font-headline text-5xl sm:text-6xl md:text-7xl font-extrabold tracking-tight text-balance">
             Aminul Islam<br /><DynamicText />
           </h1>
-          <Button size="lg" className="mt-8">Let's Talk your product</Button>
+          <Button size="lg" className="mt-8">Let's Talk Your Idea</Button>
         </div>
       </div>
       <div className="absolute bottom-10 left-1/2 -translate-x-1/2">
